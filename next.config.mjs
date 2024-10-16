56c74e9d413177833373e0f489c61e462b2f260a
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2024-10-16 08:19:13 +0200

    gallery template, next config, styles, images data template

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.fbcdn.net",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import { galleryImages } from "../utils/galleryImages";
+import { spinnerUrl } from "../constants";
+
 export default function Page() {
+  const imageWidth = 300;
+  const imageHeight = 100;
   return (
-    <div className="px-2">
-      <p>Gallery Page this will be photos</p>
+    <div className="py-2 flex flex-col justify-center">
+      <div className="gallery-box">
+        {galleryImages.map(({ id, src }) => (
+          <Image
+            key={id}
+            src={src}
+            alt="image"
+            priority={true}
+            width={imageWidth}
+            height={imageHeight}
+            blurDataURL={spinnerUrl}
+            className="w-full mb-2"
+          />
+        ))}
+      </div>
     </div>
   );
 }
