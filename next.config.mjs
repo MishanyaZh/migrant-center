ea91d0cebf5666d2c2e6c91eadf71883589c5425
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2024-11-10 15:51:32 +0100

    Refactor galleryImages to dynamically load images from the gallery directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.fbcdn.net",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/app/utils/galleryImages.ts
+++ src/app/utils/galleryImages.ts
@@ -1,52 +1,10 @@
-export const galleryImages = [
-  {
-    id: 1,
-    src: "https://scontent-waw2-1.xx.fbcdn.net/v/t51.75761-15/463066669_17865794205234334_3274677373095618709_n.jpg?stp=dst-jpg_tt7&_nc_cat=107&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=oFY-2pk2-UMQ7kNvgFdhNYc&_nc_zt=23&_nc_ht=scontent-waw2-1.xx&_nc_gid=A2l_DWfRooVE-wfyEgA6EPG&oh=00_AYCApz_iwacwbkae4wYzGufQQmNxfgq_ClQ0nCHX98G5ow&oe=67150E39",
-  },
+import fs from "fs";
+import path from "path";
 
-  {
-    id: 2,
-    src: "https://scontent-waw2-2.xx.fbcdn.net/v/t51.75761-15/462806240_17865072066234334_1901931725305565858_n.jpg?stp=dst-jpg_tt7&_nc_cat=103&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=cX05UiFSo0MQ7kNvgG8l9_6&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&_nc_gid=AwO8xXmY2N3bfjtOPWzxo45&oh=00_AYAiw3Ne3hPQgjpbunHsRk5P0FuwVRfXWj76Sfij4R-TkA&oe=67151FA0",
-  },
-  {
-    id: 3,
-    src: "https://scontent-waw2-2.xx.fbcdn.net/v/t39.30808-6/462620387_17864950734234334_6996133687222596298_n.jpg?stp=dst-jpg_tt7&_nc_cat=102&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=lo2BRsJHseoQ7kNvgFO9Fje&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&_nc_gid=A2OthlNIel8zQcN43qpkzCT&oh=00_AYDigouMvj9LcOtBJ8UvzkZY2fzqsBspRcweBFIJmxOTeQ&oe=671530D2",
-  },
-  {
-    id: 4,
-    src: "https://scontent-waw2-1.xx.fbcdn.net/v/t39.30808-6/462182013_17864677617234334_5128118502558617420_n.jpg?stp=dst-jpg_tt7&_nc_cat=108&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=gI_X0WXcuIAQ7kNvgGmDVD4&_nc_zt=23&_nc_ht=scontent-waw2-1.xx&_nc_gid=Abm6Kc0DJNwBFGMf5Nm3lUI&oh=00_AYBSgSyGYhhKSJ1Zt9Zm7G7iDbS7lxiaSUNLCCIqrgDpcg&oe=671525BE",
-  },
+const imagesDir = path.join(process.cwd(), "public", "img", "gallery");
+const imageFiles = fs.readdirSync(imagesDir);
 
-  {
-    id: 5,
-    src: "https://scontent-waw2-2.xx.fbcdn.net/v/t39.30808-6/461977740_17864241030234334_8048970868069675639_n.jpg?stp=dst-jpg_tt7&_nc_cat=106&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=mkBvN3c5d0EQ7kNvgFsmBeT&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&_nc_gid=Avt9Rg5BAqiobAxAZ_B9A4F&oh=00_AYDi8Z-PtR2ng8NpuUlxo9dyPUYsy8ceCBWGgaMlLmBZXQ&oe=67153479",
-  },
-  {
-    id: 6,
-    src: "https://scontent-waw2-1.xx.fbcdn.net/v/t39.30808-6/461924437_17864103264234334_8353805419374160580_n.jpg?stp=dst-jpg_tt7&_nc_cat=108&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=hn5zBa8t9kAQ7kNvgFDE6b8&_nc_zt=23&_nc_ht=scontent-waw2-1.xx&_nc_gid=AW_RJfwMGbMiaT6_n4sKnCs&oh=00_AYBikSDUlauFwM_lU0bbVoq7UCk9OK2SdXG_koO5EQyZSA&oe=67152151",
-  },
-  {
-    id: 7,
-    src: "https://scontent-waw2-1.xx.fbcdn.net/v/t39.30808-6/461579040_17863689897234334_3932661659400748101_n.jpg?stp=dst-jpg_tt7&_nc_cat=108&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=EQP8TKzCET0Q7kNvgETXdKq&_nc_zt=23&_nc_ht=scontent-waw2-1.xx&_nc_gid=AOOHyZRwX4_dd7P0XVa382M&oh=00_AYDnd7DkiFBjxGi3nAaR9FNa9PnL8vBykJQTX2clNNxGig&oe=671534C9",
-  },
-  {
-    id: 8,
-    src: "https://scontent-waw2-2.xx.fbcdn.net/v/t39.30808-6/461183821_17863241166234334_5587080081275763846_n.jpg?stp=dst-jpg_tt7&_nc_cat=103&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yCSmDKnrvjMQ7kNvgENBu6O&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&_nc_gid=A1SxyIS9N-41vq2-rO77GI2&oh=00_AYAE2izWCiBrBQIsZCrX6wQz3uXm3448SpwGRJGH-5XHSQ&oe=67153553",
-  },
-  {
-    id: 9,
-    src: "https://scontent-waw2-1.xx.fbcdn.net/v/t39.30808-6/461324966_17863128135234334_4219507698548276211_n.jpg?stp=dst-jpg_tt7&_nc_cat=104&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=EkI7W0Mz7pYQ7kNvgEHc112&_nc_zt=23&_nc_ht=scontent-waw2-1.xx&_nc_gid=APyecmNAPSvEDSHEASbgUDX&oh=00_AYCwwC7rm60U-Z1rns7s2FfENZuVk8TqAGd1tfavr19Jaw&oe=67152FE6",
-  },
-  {
-    id: 10,
-    src: "https://scontent-waw2-2.xx.fbcdn.net/v/t39.30808-6/461189688_17862975762234334_8750781852239561533_n.jpg?stp=dst-jpg_tt7&_nc_cat=103&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=FC3U9PbYeAoQ7kNvgHACOSM&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&_nc_gid=ALRKNX1H65Af7pHSxf4oNGe&oh=00_AYDzVmEazjAcWMvQObXJw3FXS7K95_C3DJiXRGpDpT2AJQ&oe=67153913",
-  },
-  {
-    id: 11,
-    src: "https://scontent-waw2-1.xx.fbcdn.net/v/t39.30808-6/460919358_17862716607234334_2836211809439223419_n.jpg?stp=dst-jpg_tt7&_nc_cat=111&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=9mxZdJntIjQQ7kNvgEZ3DgI&_nc_zt=23&_nc_ht=scontent-waw2-1.xx&_nc_gid=A-5Ayj8HWwGQunWIrfj4i7R&oh=00_AYDE39bgY8yeS6qWlwngHWRzlIFEEf9g7GTcFRvhbUShyQ&oe=671533D9",
-  },
-  {
-    id: 12,
-    src: "https://scontent-waw2-2.xx.fbcdn.net/v/t39.30808-6/459574437_17861728722234334_4733004734469923538_n.jpg?stp=dst-jpg_tt7&_nc_cat=106&cb=99be929b-defccdb7&ccb=1-7&_nc_sid=127cfc&_nc_ohc=ZPZ3rotwjGsQ7kNvgH349BP&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&_nc_gid=A4rozhqI_xo96hOJfUIbxay&oh=00_AYBOcczinSg6VjpZndbQO0NFhwJnXZlbWct4KsKMm__AdA&oe=6715429E",
-  },
-];
+export const galleryImages = imageFiles.map((file, index) => ({
+  id: index + 1,
+  src: `/img/gallery/${file}`,
+}));
